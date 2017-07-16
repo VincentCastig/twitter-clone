523f171e5c72ba210cf440811a62a0397e3cbda8
Vincent Castigliola <vincentcastigliola@Vincents-MacBook-Air.local>
2017-07-16 15:39:17 -0500

    final twitter clone

--- animations.js
+++ animations.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
   var $textarea = ('.tweet-compose');
-  var $existingcontent = document.getElementsByClassName("content")
-  var $existingaction = document.getElementsByClassName("tweet-actions")
-  var $existingstats = document.getElementsByClassName("stats")
-  var $existingreply = document.getElementsByClassName("reply")
+  var $existingcontent = (".content");
+  var $existingaction = document.getElementsByClassName("tweet-actions");
+  var $existingstats = document.getElementsByClassName("stats");
+  var $existingreply = document.getElementsByClassName("reply");
   var $tweet_val = $($textarea)[0];
+  
+
 
 
 
   $('#tweet-controls').css('display', 'none');
+  $('.tweet-actions').css('display', 'none');
+  $('.stats').css('display', 'none');
+  $('.tweet-actions').css('display', 'none');
+
 
   $($textarea).on('click', function(){
     $('#tweet-controls').show();
@@ -19,10 +25,8 @@ $(document).ready(function(){
     if($(this).val().length < 140){
     $('#char-count').css("color","#999")
     $('#char-count').text(139-$(this).val().length);
-
     $('button').removeClass('button:disabled').addClass('button');
   }
-
 
     if($(this).val().length >= 130){
       $('#char-count').css("color","red")
@@ -30,16 +34,22 @@ $(document).ready(function(){
     if($(this).val().length >= 140){
         $('button').addClass('button:disabled').removeClass('button');
       }
-
     });
 
     $('#tweet-submit').on('click', function(){
-      $($existingreply[0]).clone().prependTo('#stream');
-      $($existingaction[0]).clone().prependTo('#stream');
-      $($existingstats[0]).clone().prependTo('#stream');
-      $($tweet_val).clone().prependTo('#stream');
-      $($existingcontent[0]).clone().prependTo('#stream');
+      $('.tweet')[0].prepend($('.content').clone()[0],
+      ($($tweet_val).val()),
+      ($('.stats').clone()[0]),
+      ($($existingaction).clone())[0]),
+      ($('.reply').clone()[0]);
+      });
 
+      $('.content').hover(function(){
+        $(this).find('.tweet-actions').toggle("show");
+      });
+
+      $('.content').click(function(){
+        $(this).find('.stats').toggle("show");
       });
 
     });
